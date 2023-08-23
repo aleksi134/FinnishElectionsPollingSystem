d22be1b8f3bc4866fadcd5bbf4154d536eb1744b
Mika Laakkonen <mikalaa@uef.fi>
2023-08-23 19:45:14 +0300

    Chance account

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const CONTRACT_NAME = process.env.CONTRACT_NAME ||'finnishstuff.masterph33r.testnet'
+const CONTRACT_NAME = process.env.CONTRACT_NAME ||'finnishstuff.blockchaining.testnet'
 
 function getConfig(env) {
   switch (env) {
