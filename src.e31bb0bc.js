8226792f99952b4e7eef6bb59b558d5ffa669f35
Mika Laakkonen <mikalaa@uef.fi>
2023-08-23 19:48:24 +0300

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -46542,7 +46542,7 @@ __exportStar(require("./browser-connect"), exports);
 require("error-polyfill");
 
 },{"./key_stores/browser-index":"../node_modules/near-api-js/lib/key_stores/browser-index.js","./common-index":"../node_modules/near-api-js/lib/common-index.js","./browser-connect":"../node_modules/near-api-js/lib/browser-connect.js","error-polyfill":"../node_modules/error-polyfill/index.js"}],"config.js":[function(require,module,exports) {
-const CONTRACT_NAME = "dev-1692805678002-93889881919591" || 'blockvote.masterph33r.testnet';
+const CONTRACT_NAME = "dev-1692805678002-93889881919591" || 'finnishstuff.blockchaining.testnet';
 
 function getConfig(env) {
   switch (env) {
@@ -75249,7 +75249,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "47652" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46875" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
